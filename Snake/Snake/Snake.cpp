26afa7e3036ed0399427a69fd1e2447693c682bc
Neil Bickle (AMBCS) (AssocIScT) (TMIET) (ICTTech) <43986316+NeilBickle@users.noreply.github.com>
2020-03-16 09:03:49 +0000

    Added Border And Tail System
    1. Added Border System
    2. Added Tail System

--- Snake/Snake/Snake.cpp
+++ Snake/Snake/Snake.cpp
@@ -9,10 +9,12 @@ bool EndOfGame;
 const int Widthofgame = 30;
 const int Heightofgame = 30;
 int a, b, fruitA, fruitB, score, lives;
+int AddTailA[125], AddTailB[125];
+int LengthOfTail;
 enum eDirectionOfsnake { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDirectionOfsnake DIRECTION; 
 void Setupthegame() 
-{
+{	
 	EndOfGame = false;
 	DIRECTION = STOP;
 	a = Widthofgame / 2;
@@ -21,11 +23,11 @@ void Setupthegame()
 	fruitB = rand() % Heightofgame;
 	score = 0;
 	lives = 3;
+
 }
-
 void DrawTheGame()
-{
-
+{	
+	
 	system("cls");
 	for (int i = 0; i < Widthofgame+2; i++)
 		cout << "#";
@@ -41,8 +43,22 @@ void DrawTheGame()
 				cout << "O";
 			else if (i == fruitB && j == fruitA)
 				cout << "F";
-			else 
-				cout << " ";
+			else
+			{
+				bool TailPrint = false;
+				for (int c = 0; c < LengthOfTail; c++) 
+				{					
+					if (AddTailA[c] == j && AddTailB[c] == i) 
+					{
+						cout << "o";
+						TailPrint = true;
+					}
+					
+				}				
+				if (!TailPrint)
+						cout << " ";
+			}
+				
 
 			if (j == Widthofgame - 1)
 				cout << "#";
@@ -55,6 +71,7 @@ void DrawTheGame()
 	cout << endl;
 	cout << "Score:" << score << endl;
 	cout << "Lives:" << lives << endl;
+	cout << "Welcome To Snake" << endl;
 }
 void InputForTheGame() 
 {
@@ -80,6 +97,20 @@ void InputForTheGame()
 }
 void GameLogic()
 {
+	int PastA = AddTailA[0];
+	int PastB = AddTailB[0];
+	int BeforeA, BeforeB;
+	AddTailA[0] = a;
+	AddTailB[0] = b;
+	for (int i = 1; i < LengthOfTail; i++)
+	{
+		BeforeA = AddTailA[i];
+		BeforeB = AddTailB[i];
+		AddTailA[i] = PastA;
+		AddTailB[i] = PastB;
+		PastA = BeforeA;
+		PastB = BeforeB;
+	}
 	switch (DIRECTION)
 	{
 	case LEFT:
@@ -97,25 +128,48 @@ void GameLogic()
 	default:
 		break;
 	}
-	if (a > Widthofgame || a < 0 || b > Heightofgame || b < 0)
+	if (a >= Widthofgame) 
+	{
+		a = 0;
+			lives -= 1;
+	}
+	
+	else if (a < 0)
+	{
+		a = Widthofgame - 1;
 		lives -= 1;
+	}
+	if (b >= Heightofgame) 
+	{
+		b = 0;
+		lives -= 1;
+	}
+	else if (b < 0)
+	{
+		b = Heightofgame - 1;
+		lives -= 1;
+	}
+	for (int i = 0; i < LengthOfTail; i++)
+	{
+		if (AddTailA[i] == a && AddTailB[i] == b)
+			lives -= 1;
+	}
 	if (lives == 0)
 		EndOfGame = true;
-	if (a == fruitA && b == fruitB) {
+	if (a == fruitA && b == fruitB) {		
 		score += 10;
 		fruitA = rand() % Widthofgame;
 		fruitB = rand() % Heightofgame;
+		LengthOfTail++;
 	}
 		
 }
 int main()
 {
-	
-	
-
-	Setupthegame();
+		
+	Setupthegame();	
 	while (!EndOfGame)
-	{		
+	{	
 		DrawTheGame();
 		InputForTheGame();
 		GameLogic();
